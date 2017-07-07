8ddef0426e54987864de4ed4e3e849721e3167a2
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-07-07 16:16:42 -0500

    die generate on btn click

--- script.js
+++ script.js
@@ -32,6 +32,14 @@ function Dice() {
   }
 }
 
+function generateDie() {
+    var box = document.createElement('div');
+    box.className = 'box';
+    box.id = boxCount;
+    boxCount++;  
+    document.body.appendChild(box);
+}
+var boxCount = 0;
 var dice1 = new Dice();
 var dice2 = new Dice();
 var dice3 = new Dice();
@@ -40,11 +48,7 @@ function rollOnce() {
   dice1.roll();
   dice2.roll();
   dice3.roll();
-  dice3.roll();
-  dice3.roll();
-  dice3.roll();
-  dice3.roll();
-  dice3.roll();
+
   document.getElementById("paragraph1").innerHTML = 
         '<button class="'+dice1.className()+'" onclick="dice1.keep(); this.className=dice1.className();">'+dice1.value+'</button> ' +
         '<button class="'+dice2.className()+'" onclick="dice2.keep(); this.className=dice2.className();">'+dice2.value+'</button> ' +
