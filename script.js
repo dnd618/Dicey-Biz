3c32d0d90f0a4ea2ba6e3a5a61e09de310657ab8
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-07-07 09:34:46 -0500

    semi-functioning die and button

--- script.js
+++ script.js
@@ -1,65 +1,34 @@
 //Make DIV appear when btn clicked
-function showDiv(toggle){
-document.getElementById(toggle).style.display = 'block';
-}
-//
-// function Thrower(dice){
-//   this.myDice = dice;
-//   this.roll = function(){
-//     var sum = 0;
-//     for(var i=0; i<this.myDice.length; i++){
-//       sum += this.myDice[i].roll();
-//     }
-//     return sum;
-//   }
-//   this.addDie = function(dice){
-// 	this.myDice[this.myDice.length] = dice;
-//   }
+// function showDiv(toggle){
+//     document.getElementById(toggle).style.display = 'block';
 // }
-// function Dice(maxNum){
-//   this.number;
-//   if(maxNum){
-// 	this.max = maxNum;				
-//   }
-//   else{
-//     this.max = 6;
-//   }
-//   this.roll = function(){
-//     this.number = Math.round(Math.random()*(this.max-1))+1;
-//     return this.number;
-//   }
-//   this.getVal = function(){
-//     return this.number;
-//   }
-// }
-// var dice = [new Dice(), new Dice()];
-// var hands = new Thrower(dice);
-//   hands.addDie(new Dice());
-// for(var i=0; i<100; i++){
-//   document.write("Dice roll "+(i+1)+" = "+hands.roll()+"<br>");
-// }
-//function if # is over 6
-// for(var i=1; i<this.myDice.length; i++)
-var score = 0;
-var maxScore = 50;
-var rolls = 0;
-var maxRolls = 20;
- 
 
-function rollDice()
-{
-    var x = Math.floor( Math.random() * 6 ) + 1;
-    var y = Math.floor( Math.random() * 6 ) + 1;
- 
-    if( x == y )
-    {
-        score = getScore( x );
-        alert("You threw a Double " + x + " Your Score is "+ score);
+function Dice(container) {
+  this.button = document.createElement("button");
+  this.button.innerHTML = 0;
+  document.getElementById(container).appendChild(this.button);
+  this.button.addEventListener('click', function() {
+    this.className = 'kept';
+  });
+  this.roll = function() {
+    if(this.button.className != 'kept') {
+      this.button.innerHTML = Math.floor(Math.random()*6)+1;
     }
-    else
-    {
-        alert("You threw a " + x + " and a " + y + " Your Score is " + score);
-    }
- 
-    rolls++;
-};
+  }
+}
+var dice1;
+var dice2;
+var dice3;
+document.onreadystatechange = function () {
+  if(document.readyState == "complete") {
+    dice1 = new Dice("paragraph1");
+    dice2 = new Dice("paragraph1");
+    dice3 = new Dice("paragraph1");
+    rollOnce();
+  }
+}
+function rollOnce() { 
+  dice1.roll();
+  dice2.roll();
+  dice3.roll();
+}
