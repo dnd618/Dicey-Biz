63d1a83bdff90a3c51208d4635782853ecce4685
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-07-07 14:46:51 -0500

    roll btn works

--- script.js
+++ script.js
@@ -40,6 +40,11 @@ function rollOnce() {
   dice1.roll();
   dice2.roll();
   dice3.roll();
+  dice3.roll();
+  dice3.roll();
+  dice3.roll();
+  dice3.roll();
+  dice3.roll();
   document.getElementById("paragraph1").innerHTML = 
         '<button class="'+dice1.className()+'" onclick="dice1.keep(); this.className=dice1.className();">'+dice1.value+'</button> ' +
         '<button class="'+dice2.className()+'" onclick="dice2.keep(); this.className=dice2.className();">'+dice2.value+'</button> ' +
