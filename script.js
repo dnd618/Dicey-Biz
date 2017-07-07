5e415a7272cb909551b12c0d3ccefb0cbfd56797
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-07-07 09:50:59 -0500

    dice roll, less code

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-//Make DIV appear when btn clicked
-// function showDiv(toggle){
-//     document.getElementById(toggle).style.display = 'block';
+//Generate die 
+// function generateDie(toggle){
+//     document.getElementById(die).style.display = 'block';
 // }
-
-function Dice(container) {
-  this.button = document.createElement("button");
-  this.button.innerHTML = 0;
-  document.getElementById(container).appendChild(this.button);
-  this.button.addEventListener('click', function() {
-    this.className = 'kept';
-  });
+//Sum
+// function sum(toggle){
+//     document.getElementById(die).style.display = 'block';
+// }
+function Dice() {
+  this.value = 0;
+  this.kept = false;
   this.roll = function() {
-    if(this.button.className != 'kept') {
-      this.button.innerHTML = Math.floor(Math.random()*6)+1;
-    }
+    if(!this.kept) this.value = Math.floor(Math.random()*6)+1;
+  };
+  this.keep = function(button) {
+    this.kept = true;
+    button.className = 'kept';
   }
 }
-var dice1;
-var dice2;
-var dice3;
-document.onreadystatechange = function () {
-  if(document.readyState == "complete") {
-    dice1 = new Dice("paragraph1");
-    dice2 = new Dice("paragraph1");
-    dice3 = new Dice("paragraph1");
-    rollOnce();
-  }
-}
+
+var dice1 = new Dice();
+var dice2 = new Dice();
+var dice3 = new Dice();
+
 function rollOnce() { 
   dice1.roll();
   dice2.roll();
   dice3.roll();
+  document.getElementById("paragraph1").innerHTML = 
+        '<button id="diceOne" class="'+(dice1.kept?'kept':'keep')+'" onclick="dice1.keep(this)">'+dice1.value+'</button> ' +
+        '<button id="diceTwo" class="'+(dice2.kept?'kept':'keep')+'" onclick="dice2.keep(this)">'+dice2.value+'</button> ' +
+        '<button id="diceThree" class="'+(dice3.kept?'kept':'keep')+'" onclick="dice3.keep(this)">'+dice3.value+'</button> ';
 }
+rollOnce();
